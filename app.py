ff8510a2d42bbc9c9267a211ce325710eff95e42
Amar8769 <amarhvaid@gmail.com>
2025-04-23 12:08:55 +0530

    removed all deps

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return "Welcome to the Discount Calculator API!"
+
 
 @app.route('/calculate_discount', methods=['POST'])
 def calculate_discount():
@@ -30,9 +33,11 @@ def calculate_discount():
     except ValueError:
         return jsonify({'error': 'Invalid input values'}), 400
 
+
 @app.route('/frontend')
 def frontend():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
